bb18f629c74339344c748f240c0376299ea49b3c
nasyasakhi <nasyasakhi6@gmail.com>
2024-12-01 11:50:07 +0700

    change the route

---
+++ lib/ui/splash/components/splash_content.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SplashContent extends StatelessWidget {
+  const SplashContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
