b8fd88ca94f3f4566c1bed6b32a2e961120c91fd
Tsabitah59 <tsabitahsafa06@gmail.com>
2024-12-01 11:10:36 +0700

    adding const & size config

--- lib/const.dart
+++ lib/const.dart
@@ -1,1 +1,51 @@
-// halo
+import 'package:flutter/material.dart';
+
+// =============== COLORS ===============
+const primaryColor = Color(0xFF6BC75B);
+const secondaryColor = Color(0xFFFF8473);
+const secondaryColorThin = Color(0xFFFFC0B8);
+
+const textColor = Color(0xFF333335);
+const textColorSmall = Color(0xFF8C8C8C);
+
+// =============== TEXT ===============
+const mainTitle = TextStyle(
+  color: primaryColor,
+  fontSize: 24,
+  fontWeight: FontWeight.w600
+);
+
+const subMainTitle = TextStyle(
+  color: textColorSmall,
+  fontSize: 16,
+);
+
+const subTitle = TextStyle(
+  color: textColor,
+  fontSize: 22,
+);
+
+const linkText = TextStyle(
+  color: secondaryColor,
+  fontSize: 14,
+);
+
+const cardTitle = TextStyle(
+  color: textColor,
+  fontSize: 14,
+);
+
+const cardText = TextStyle(
+  color: textColor,
+  fontSize: 12,
+);
+
+// =============== SIZE & DURATION ===============
+// Animation Duration
+const animationDuration = Duration(milliseconds: 200);
+
+// Value for Padding Size
+const defaultPadding = 20.0;
+
+// Border Radius
+const borderRadiusSizeMine = 18.0;
